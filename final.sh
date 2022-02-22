53562956c6dafcd43b1581d64adc5e7c1e8308f6
prashant <dongreprashu003@gmail.com>
2022-02-22 21:42:41 +0530

    final.sh

---
+++ final.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+function checkpalindromo()
+{
+sum=0
+temp=0
+temp=$string
+while(($string !=0))
+do
+	reminder=$string%10
+	((sum=$sum*10+$rimender))
+	string=$string/10
+done
+if [ $temp -eq $sum ]
+then
+	echo $sum
+else
+	echo "string is not palindrome"
+fi
+}
+read -p "enter a no to palindrome" number
+res1="$( checkpalinndrome $(($string)) )"
+if [ $res1 -eq $res2 ]
+then
+ 	echo "two are palindrome to each other"
+else
+	echo "two no are not palindrome to each other"
+fi
