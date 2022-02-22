373ef8a1163b9e912b8d26866902446a0dac1545
prashant <dongreprashu003@gmail.com>
2022-02-22 19:45:54 +0530

    day5floreproblems

---
+++ day5floreproblems/PowerOf2.sh
@@ -0,0 +1,11 @@
+##!/bin/bash -x
+
+
+read -p "enter a number " n
+
+for (( counter=1; counter<=n; counter++ ))
+do
+     result=$((2*$counter))
+     echo -n $result
+done
+    printf "\n"
---
+++ day5floreproblems/computesFactorialno.sh
@@ -0,0 +1,13 @@
+##!/bin/bash -x
+
+ 
+read -p "enter a number " number
+fact=1
+for (( i=1; i<=$number; i++ ))
+do
+    fact=$(($fact*$i));
+	 
+done
+
+	 echo "The factorial of $number is:"
+    echo $fact
